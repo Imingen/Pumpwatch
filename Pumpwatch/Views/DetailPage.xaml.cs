4a9ea505c7d6d09c69653de58d1dd8c6f11d48db
Imingen <marius.steller@gmail.com>
2016-11-20 15:51:25 +0100

    UWP app working no major errors

---
+++ Pumpwatch/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using Pumpwatch.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Pumpwatch.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
