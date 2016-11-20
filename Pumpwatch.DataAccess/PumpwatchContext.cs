63eb20050bda5426ebdd3667e3d88fea7515b711
Imingen <marius.steller@gmail.com>
2016-11-20 15:07:32 +0100

    Conetx OK i fguess

---
+++ Pumpwatch.DataAccess/PumpwatchContext.cs
@@ -0,0 +1,33 @@
+﻿using Pumpwatch.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pumpwatch.DataAccess
+{
+    public class PumpwatchContext : DbContext
+    {
+        public PumpwatchContext() :
+            base("PumwatchTest")
+        {
+            this.Configuration.ProxyCreationEnabled = false;
+        }
+
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<Workout> Workouts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Workout>().HasMany<Exercise>(e => e.Exercises).WithMany(w => w.workouts).Map(cs =>
+            {
+                cs.MapLeftKey("WorkoutId");
+                cs.MapRightKey("ExerciseId");
+                cs.ToTable("WorkoutWithExercises");
+            });
+        }
+    }
+}
