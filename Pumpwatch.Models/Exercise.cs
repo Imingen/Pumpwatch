9556c5b86b4db8047bcdb48018754d8998ad163c
Imingen <marius.steller@gmail.com>
2016-11-20 15:00:45 +0100

    Models OK

---
+++ Pumpwatch.Models/Exercise.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pumpwatch.Models
+{
+    public enum Categories
+    {
+        Chest,
+        Back,
+        Shoulder,
+        Legs,
+        Arms
+    }
+
+    public class Exercise
+    {
+        [Key]
+        public int ExerciseId { get; set; }
+        [Required]
+        public string ExerciseName { get; set; }
+        public string ExersiceDescription { get; set; }
+        public byte[] Image { get; set; }
+        public Categories Category { get; set; }
+        public virtual ICollection<Workout> workouts { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{ExerciseName} - {Category}\n{ExersiceDescription}";
+        }
+
+    }
+}
---
+++ Pumpwatch.Models/Workout.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pumpwatch.Models
+{
+    public class Workout
+    {
+        [Key]
+        public int WorkoutId { get; set; }
+        [Required]
+        public string WorkoutName { get; set; }
+
+        public virtual ICollection<Exercise> Exercises { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{WorkoutName}\n{Exercises}";
+        }
+    }
+}
