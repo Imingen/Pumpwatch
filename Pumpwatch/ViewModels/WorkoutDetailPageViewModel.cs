8f936a89f3c8014e87126439361b5174da394160
Imingen <marius.steller@gmail.com>
2016-11-22 13:10:22 +0100

    WrokoutDetailPage.xaml OK

---
+++ Pumpwatch/ViewModels/WorkoutDetailPageViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Pumpwatch.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+
+namespace Pumpwatch.ViewModels
+{
+    public class WorkoutDetailPageViewModel : ViewModelBase, INotifyPropertyChanged
+    {
+
+        private string workoutName;
+        public string WorkoutName
+        {
+            get { return workoutName; }
+            set
+            {
+                if (value != this.workoutName)
+                {
+                    workoutName = value;
+                    NotifyPropertyChanged("WorkoutName");
+                }
+            }
+        }
+
+        public Workout w1 { get; set; }
+
+
+
+
+
+        public new event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public void GotoDetailsPage() =>
+            NavigationService.Navigate(typeof(Views.DetailPage));
+
+        public void GotoSettings() =>
+            NavigationService.Navigate(typeof(Views.SettingsPage), 0);
+
+        public void GotoPrivacy() =>
+            NavigationService.Navigate(typeof(Views.SettingsPage), 1);
+
+        public void GotoAbout() =>
+            NavigationService.Navigate(typeof(Views.SettingsPage), 2);
+    }
+}
